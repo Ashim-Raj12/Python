ca9874015f959f2f1f87dcda788908876d16575b
Ashim Raj <programwithashim@gmail.com>
2026-02-12 17:58:42 +0530

    Add main3.py for conditional statements using if-else structure

---
+++ main3.py
@@ -0,0 +1,9 @@
+# Conditional Statements
+
+"""If Else Statements"""
+a = int(input("Enter a number : "))
+
+if a > 10:
+    print(f"{a} is greater than 10")
+else:
+    print(f"{a} is smaller than 10")
