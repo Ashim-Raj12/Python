ab9448ceceaf64832f9140a5efae600543385842
Ashim Raj <programwithashim@gmail.com>
2026-02-12 18:06:29 +0530

    Fix typo in docstring and format conditional statements in main3.py

--- main3.py
+++ main3.py
@@ -1,9 +1,20 @@
 # Conditional Statements
 
-"""If Else Statements"""
+"""If Else Statement"""
 a = int(input("Enter a number : "))
 
 if a > 10:
     print(f"{a} is greater than 10")
 else:
     print(f"{a} is smaller than 10")
+
+
+"""If Elif Else Statement"""
+money = int(input("Enter the amount of money : "))
+
+if money == 10:
+    print("Chocobar")
+elif money == 20:
+    print("Big Chocobar")
+else:
+    print("Cone")
