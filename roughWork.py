187b138c75d4450de9c093c132baafbcbc6f48c3
Ashim Raj <programwithashim@gmail.com>
2026-02-12 18:27:36 +0530

    Add gender greeting functionality to roughWork.py

--- roughWork.py
+++ roughWork.py
@@ -9,3 +9,15 @@ elif numB > numA:
     print(f"{numB} is greater than {numA}")
 else:
     print(f"{numA} is equal to {numB}")
+
+# Q2. Accept the gender from the user as char and print the respective greeting message .Ex - Good Morning Sir (on the basis of gender)
+
+gender = input("Enter F or M as your gender : ").strip().upper()
+
+if gender == "M":
+    print("Good Morning Sir")
+elif gender == "F":
+    print("Good Morning Mam")
+else:
+    print("Please Enter a valid gender")
+    print("Restart to see results")
