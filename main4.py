64f44b61cef4fa97e1abc9e679697940bdc47bb4
Ashim Raj <programwithashim@gmail.com>
2026-02-13 20:44:37 +0530

    Refactor loops in main4.py to use a single user input for even number printing

--- main4.py
+++ main4.py
@@ -1,12 +1,19 @@
 # Loops
 
-# For Loop -- Printing table of the user input
+# For Loop
 num = int(input("Enter a number : "))
 
 for i in range(1, 11, 1):
     print(f"{num} * {i} = {num * i}")
 
-numA = int(input("Enter another number : "))
-
-for i in range(0, numA, 2):
+for i in range(0, num, 2):
     print(i)
+
+# For Loops for String
+name = "Ashim Raj"
+
+for i in range(0, len(name), 2):
+    print(name[i])
+
+for char in name:
+    print(char)
