b5b8b11065778611049ab533c223e657930da776
Ashim Raj <programwithashim@gmail.com>
2026-02-13 18:56:17 +0530

    Add initial implementation of loops for printing messages and numbers in roughWork2.py

---
+++ roughWork2.py
@@ -0,0 +1,17 @@
+# Accept an integer and Print hello world n times
+num = int(input("Enter a number : "))
+
+for i in range(1, num + 1):
+    print("Hello World")
+
+# Print natural number up to n
+for i in range(1, num + 1):
+    print(i)
+
+# Reverse for loop. Print n to 1
+for i in range(num, 0, -1):
+    print(i)
+
+# Take a number as input and print its table
+for i in range(1, 11):
+    print(f"{num} * {i} = {num * i}")
