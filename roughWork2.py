beb23508633d3e9b70b3bf340a425e5c7ec8d996
Ashim Raj <programwithashim@gmail.com>
2026-02-13 21:23:43 +0530

    Refactor loop to print "Hello World" n times based on user input

--- roughWork2.py
+++ roughWork2.py
@@ -1,7 +1,7 @@
 # Accept an integer and Print hello world n times
 num = int(input("Enter a number : "))
 
-for i in range(1, num + 1):
+for i in range(num):
     print("Hello World")
 
 # Print natural number up to n
@@ -15,3 +15,19 @@ for i in range(num, 0, -1):
 # Take a number as input and print its table
 for i in range(1, 11):
     print(f"{num} * {i} = {num * i}")
+
+# Sum upto n terms
+count = 0
+
+for i in range(1, num + 1):
+    count = count + i
+
+print(f"Sum is {count}")
+
+# Factorial of a number
+result = 1
+
+for i in range(1, num + 1):
+    result = result * i
+
+print(f"Factorial is {result}")
