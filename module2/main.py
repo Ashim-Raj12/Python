4cbf2414632d49089b45664689a7d50f7efe8a8a
Ashim Raj <programwithashim@gmail.com>
2026-02-14 18:13:19 +0530

    add product function

--- module2/main.py
+++ module2/main.py
@@ -12,5 +12,11 @@ def sum(a, b):
     print(f"The sum is {sum}")
 
 
+def product(a=1, b=1):
+    product = a * b
+    print(f"The product is {product}")
+
+
 hello("Ashim")
 sum(12, 54)
+product(b=4, a=5)
