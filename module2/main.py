b78db34ed2b76887d54537b789eda9f1195b9558
Ashim Raj <programwithashim@gmail.com>
2026-02-14 18:24:17 +0530

    Pallindrome check add

--- module2/main.py
+++ module2/main.py
@@ -17,6 +17,18 @@ def product(a=1, b=1):
     print(f"The product is {product}")
 
 
+def pallindrome(st):
+    rev = ""
+    for char in st:
+        rev = char + rev
+    if rev == st:
+        print(f"{st} is pallindrome")
+    else:
+        print(f"{st} is not a pallindrome")
+
+
 hello("Ashim")
 sum(12, 54)
 product(b=4, a=5)
+pallindrome("NAMAN")
+pallindrome("ASHIM")
